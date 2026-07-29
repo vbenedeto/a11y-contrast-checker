507d1ebe62260cedcca44d5eef47e78057029269
Vittoria <vbenedetoamerico@gmail.com>
2026-07-29 11:35:59 +0200

    feat: add WCAG contrast calculation engine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import EmotionRegistry from "@/lib/emotion/emotion-registry";
 import "./globals.css";
-import EmotionRegistry from "@/lib/emotion-registry";
-import { ThemeProvider } from "@emotion/react";
-import theme from "@/lib/theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
